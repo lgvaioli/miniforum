7a859754bf3d59cb6653784c501027b1b1d21b3d
Laureano G. Vaioli <laureano3400@gmail.com>
2020-08-26 17:29:52 -0300

    Fix Kubernetes deploy
    Note that the database tables still have to be manually created,
    so we're not quite there yet

--- k8s/k8s_destroy.sh
+++ k8s/k8s_destroy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 kubectl delete deployment miniforum
+kubectl delete statefulset miniforum-statefulset
 kubectl delete service miniforum-service
-kubectl delete service postgres-service
+kubectl delete service miniforum-postgres-service
 kubectl delete persistentvolume miniforum-persistent-volume
 kubectl delete persistentvolumeclaim miniforum-persistent-volume-claim
-kubectl delete statefulset miniforum-statefulset
 kubectl delete configmap miniforum-configmap
